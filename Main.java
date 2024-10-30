faa2f0545a2e3f0866cb42c57c4cad941e6705fc
Hana574 <hanag10@nycstudents.net>
2024-10-30 16:30:26 +0000

    sumPath in progress

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] nums = {
+            {12, 3, 4, 13, 5},
+            {11, 21, 2, 14, 16},
+            {7, 8, 9, 15, 0},
+            {10, 17, 20, 19, 1},
+            {18, 22, 30, 25, 6}
+        };
+        GridPath z = new GridPath(nums);
+        int sum = 0;
+        Location loc = z.getNextLoc(3, 3);
+        sum = sum + nums[4][3];
+        System.out.print(sum);
+    }
+}
